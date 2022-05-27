4e1de3dd8788cc8b3f66b472dcf60d0c5ec7e6f2
Wilsoon <wilsonoon2018@gmail.com>
2022-05-27 11:12:44 +0800

    Code Modifications

--- src/main/java/com/matchamc/core/bukkit/BukkitMain.java
+++ src/main/java/com/matchamc/core/bukkit/BukkitMain.java
@@ -1,16 +1,32 @@
 package com.matchamc.core.bukkit;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.messaging.PluginMessageListener;
 
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteStreams;
 import com.matchamc.shared.util.MsgUtils;
 
-public class BukkitMain extends JavaPlugin {
+public class BukkitMain extends JavaPlugin implements PluginMessageListener {
 	public static final String CONSOLE_PLUGIN_NAME = "MatchaMC-Bukkit";
 	public static final String PLUGIN_PREFIX = MsgUtils.color("&9&l[&3&lMatchaMC&9&l] &r");
 	public static final String NON_PLAYER_ERROR = PLUGIN_PREFIX + MsgUtils.color("&cYou must be a player to execute this command.");
-	
+
 	@Override
 	public void onEnable() {
 		MsgUtils.sendBukkitConsoleMessage("&aEnabling MatchaMC [Bukkit/Spigot] version " + getDescription().getVersion());
+	}
+
+	// All subservers must have this plugin or else they are unable to receive messages from /sendtoall.
+	@Override
+	public void onPluginMessageReceived(String channel, Player player, byte[] message) {
+		if(!channel.equals("BungeeCord"))
+			return;
+		ByteArrayDataInput in = ByteStreams.newDataInput(message);
+		String subchannel = in.readUTF();
+		if(!subchannel.equals("Forward"))
+			return;
+
 	}
 }
--- src/main/java/com/matchamc/core/bungee/commands/SendToAllCmd.java
+++ src/main/java/com/matchamc/core/bungee/commands/SendToAllCmd.java
@@ -1,8 +1,24 @@
 package com.matchamc.core.bungee.commands;
 
+import com.matchamc.core.bungee.BungeeMain;
+
+import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.plugin.Command;
 
 public class SendToAllCmd extends Command {
 	// Issue a command to all servers
+	public SendToAllCmd() {
+		super("sendtoall");
+
+	}
+
+	@Override
+	public void execute(CommandSender sender, String[] args) {
+		if(!(sender.hasPermission("core.bungee.sendtoall"))) {
+			sender.sendMessage(BungeeMain.NO_PERMISSION_ERROR);
+			return;
+		}
+		String command = String.join(" ", args).trim();
+	}
 
 }
