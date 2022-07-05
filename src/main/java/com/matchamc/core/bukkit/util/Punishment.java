2615fa35683d1f949ab88022d42779c1482dce35
Wilsoon <wilsonoon2018@gmail.com>
2022-07-05 10:01:22 +0800

    Code Modifications

--- src/main/java/com/matchamc/core/bukkit/util/Punishment.java
+++ src/main/java/com/matchamc/core/bukkit/util/Punishment.java
@@ -3,14 +3,28 @@ package com.matchamc.core.bukkit.util;
 import java.time.Duration;
 import java.util.UUID;
 
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import com.matchamc.core.bukkit.BukkitMain;
+import com.matchamc.shared.MsgUtils;
+
+import litebans.api.Database;
+
 public class Punishment {
+	public static boolean cancel = false;
+	private BukkitMain instance;
+	private PlayerRegistrar registrar;
 	private Type type;
 	private UUID executor, punished;
 	private String reason;
 	private Duration duration;
+	private boolean executed = false;
 
-	public Punishment(Type type, UUID executor, UUID punished, String reason, Duration duration) {
+	public Punishment(BukkitMain instance, PlayerRegistrar registrar, Type type, UUID executor, UUID punished, String reason, Duration duration) {
 		// specify null duration for forever
+		this.instance = instance;
+		this.registrar = registrar;
 		this.type = type;
 		this.executor = executor;
 		this.punished = punished;
@@ -19,14 +33,50 @@ public class Punishment {
 	}
 
 	public void executePunishment() {
+		Database database = Database.get();
+		String additional_arguments = "--sender=" + (Punishments.PLUGIN_PUNISHMENT_NAME.replace("%executor%", registrar.getNameFromRegistrar(executor)) + " --sender-uuid=" + executor.toString());
 		switch(type) {
 		case BAN:
+			new BukkitRunnable() {
+				@Override
+				public void run() {
+					Punishment.cancel = database.isPlayerBanned(punished, null);
+				}
+			}.runTaskAsynchronously(instance);
+			if(Punishment.cancel) {
+				Bukkit.getPlayer(executor).sendMessage(MsgUtils.color("&cThis player is already banned."));
+				Punishment.cancel = false;
+				return;
+			}
+			if(duration == null)
+				Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "ban " + punished + "-s " + reason + " " + additional_arguments);
+			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "ban " + punished + "-s " + duration.toDays() + "d " + reason + " " + additional_arguments);
+			executed = true;
 			break;
 		case MUTE:
+			new BukkitRunnable() {
+				@Override
+				public void run() {
+					Punishment.cancel = database.isPlayerMuted(punished, null);
+				}
+			}.runTaskAsynchronously(instance);
+			if(Punishment.cancel) {
+				Bukkit.getPlayer(executor).sendMessage(MsgUtils.color("&cThis player is already muted."));
+				Punishment.cancel = false;
+				return;
+			}
+			if(duration == null)
+				Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "mute " + punished + "-s " + reason + " " + additional_arguments);
+			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "mute " + punished + "-s " + duration.toDays() + "d " + reason + " " + additional_arguments);
+			executed = true;
 			break;
 		case WARN:
+			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "warn -s " + punished + " " + reason + " " + additional_arguments);
+			executed = true;
 			break;
 		case KICK:
+			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "kick -s " + punished + " " + reason + " " + additional_arguments);
+			executed = true;
 			break;
 		}
 	}
@@ -49,6 +99,10 @@ public class Punishment {
 
 	public Duration getDuration() {
 		return duration;
+	}
+
+	public boolean isExecuted() {
+		return executed;
 	}
 
 	public void setExecutor(UUID executor) {
--- src/main/java/com/matchamc/core/bukkit/util/Punishments.java
+++ src/main/java/com/matchamc/core/bukkit/util/Punishments.java
@@ -6,14 +6,15 @@ import java.util.UUID;
 import com.matchamc.core.bukkit.BukkitMain;
 
 public class Punishments {
-	private static final String PLUGIN_PUNISHMENT_NAME = "MatchaMC Punishments (%executor%)";
-	private static final UUID PLUGIN_PUNISHMENT_UUID = UUID.fromString("740c1ecf-8cde-4c24-8b02-9ffbc76ff7f2");
+	public static final String PLUGIN_PUNISHMENT_NAME = "MatchaMC Punishments (%executor%)";
+	public static final UUID PLUGIN_PUNISHMENT_UUID = UUID.fromString("740c1ecf-8cde-4c24-8b02-9ffbc76ff7f2");
 	private BukkitMain instance;
+
 	public Punishments(BukkitMain instance) {
 		this.instance = instance;
 	}
-	
+
 	public Punishment createPunishment(Punishment.Type type, UUID executor, UUID punished, String reason, Duration duration) {
-		return new Punishment(type, executor, punished, reason, duration);
+		return new Punishment(instance, type, executor, punished, reason, duration);
 	}
 }
