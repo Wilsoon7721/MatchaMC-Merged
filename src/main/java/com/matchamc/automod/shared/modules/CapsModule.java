23e4c1b70b648a2f80c5a3c2fd551fe57a9ee441
Wilsoon <wilsonoon2018@gmail.com>
2022-07-12 06:59:49 +0800

    Update CapsModule.java

--- src/main/java/com/matchamc/automod/shared/modules/CapsModule.java
+++ src/main/java/com/matchamc/automod/shared/modules/CapsModule.java
@@ -1,7 +1,37 @@
 package com.matchamc.automod.shared.modules;
 
+import com.matchamc.automod.shared.ChatPlayer;
 import com.matchamc.automod.shared.Module;
 
 public class CapsModule implements Module {
+	private boolean enabled;
+	private boolean replace;
+	private int maxCaps;
+	private int maxWarns;
+	private String warnNotification;
+
+	public void loadModule() {
+
+	}
+
+	@Override
+	public boolean meetsCondition(ChatPlayer player, String message) {
+
+	}
+
+	@Override
+	public int getMaxWarnings() {
+
+	}
+
+	@Override
+	public String getModuleName() {
+		return "Caps";
+	}
+
+	@Override
+	public String getWarnNotification() {
+
+	}
 
 }
