c1cb469b6a2fc65fef67f4695570591e7bdabd28
Wilsoon <wilsonoon2018@gmail.com>
2022-08-01 16:10:43 +0800

    Minor - Code Modifications

--- src/main/java/com/matchamc/automod/bukkit/Violations.java
+++ src/main/java/com/matchamc/automod/bukkit/Violations.java
@@ -3,6 +3,7 @@ package com.matchamc.automod.bukkit;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Set;
 import java.util.UUID;
 
@@ -10,6 +11,7 @@ import com.matchamc.shared.MsgUtils;
 
 public class Violations {
 	private AutoMod autoMod;
+	private HashMap<UUID, Set<Violation>> playerViolations = new HashMap<>();
 	private Connection con; // Database connection to store violations
 
 	public Violations(AutoMod autoMod) {
@@ -17,7 +19,10 @@ public class Violations {
 	}
 
 	public Set<Violation> getViolations(UUID uuid, Module module) {
-
+		Set<Violation> violations = playerViolations.get(uuid);
+		if(violations.isEmpty())
+			return null;
+		return violations;
 	}
 
 	public void setupDatabase(String host, int port, String databaseName, String username, String password) {
@@ -26,6 +31,7 @@ public class Violations {
 			con = DriverManager.getConnection(url, username, password);
 		} catch(SQLException ex) {
 			sql(ex);
+			return;
 		}
 	}
 
@@ -38,13 +44,12 @@ public class Violations {
 			e.printStackTrace();
 			return null;
 		}
-
 	}
 
 	private void sql(SQLException ex) {
 		MsgUtils.sendBukkitConsoleMessage("&c[Violations - Database] Encountered an SQL Exception");
-		MsgUtils.sendBukkitConsoleMessage("&c[Error Message] " + ex.getMessage());
-		MsgUtils.sendBukkitConsoleMessage("&c[Error Code] " + ex.getErrorCode());
-		MsgUtils.sendBukkitConsoleMessage("&c[SQL State] " + ex.getSQLState());
+		MsgUtils.sendBukkitConsoleMessage("&c  - Error Message: " + ex.getMessage());
+		MsgUtils.sendBukkitConsoleMessage("&c  - Error Code: " + ex.getErrorCode());
+		MsgUtils.sendBukkitConsoleMessage("&c  - SQL State: " + ex.getSQLState());
 	}
 }
