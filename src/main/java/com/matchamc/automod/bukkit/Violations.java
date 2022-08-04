18cafce6be53d41a80f1c56bea40802c9d41bdb0
Wilsoon <wilsonoon2018@gmail.com>
2022-08-04 15:13:21 +0800

    Merge changes to code

--- src/main/java/com/matchamc/automod/bukkit/Violations.java
+++ src/main/java/com/matchamc/automod/bukkit/Violations.java
@@ -1,7 +1,10 @@
 package com.matchamc.automod.bukkit;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Set;
@@ -32,7 +35,18 @@ public class Violations {
 	public void setupDatabase(String host, int port, String databaseName, String username, String password) {
 		String url = "jdbc:mysql://" + host + ":" + port + "/" + databaseName;
 		try {
-			con = DriverManager.getConnection(url, username, password);
+			Connection con = DriverManager.getConnection(url, username, password);
+			if(!con.isValid(30))
+				throw new SQLException("Connection invalidated after 30 seconds", "INVALIDATED", -1);
+			this.con = con;
+			DatabaseMetaData dbm = this.con.getMetaData();
+			ResultSet tables = dbm.getTables(null, null, "violations", null);
+			if(!tables.next()) {
+				PreparedStatement stmt = con.prepareStatement("CREATE TABLE violations(id int AUTO_INCREMENT, violator_uuid varchar(255), module varchar(255))");
+				stmt.executeUpdate();
+				MsgUtils.sendBukkitConsoleMessage("&e[Violations - Database] Executed database update to create violations table.");
+				stmt.close();
+			}
 		} catch(SQLException ex) {
 			sql(ex);
 			return;
